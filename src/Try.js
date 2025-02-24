356ef266cfc333886895f4f928ad56ef7bd5f01b
Michel <michelmunezero25@gmail.com>
2025-02-25 00:23:45 +0200

    Update files and removed unnecessary assets

--- src/Try.js
+++ src/Try.js
@@ -23,6 +23,13 @@ function Try() {
           </div>
         </div>
       </div>
+      <div class="attribution">
+        Challenge by{" "}
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Ojochenemi</a>.
+      </div>
     </div>
   );
 }
